bf39509d73dc38950f6994114008e4225a48d9e8
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2020-06-14 08:39:45 +0200

    Database migrated with postgres

--- project/project/settings.py
+++ project/project/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': 'postgres',
+        'PASSWORD': '2265',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
